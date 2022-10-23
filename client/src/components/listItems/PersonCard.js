4548813d40a0e8461e83e6f24b43875717c42a7f
Aleirbag04 <94083464+Aleirbag04@users.noreply.github.com>
2022-10-23 16:36:54 -0700

    feat: person card

--- client/src/components/listItems/PersonCard.js
+++ client/src/components/listItems/PersonCard.js
@@ -1,13 +1,71 @@
-import { Card } from "antd";
+import { EditOutlined } from "@ant-design/icons";
+import { Button, Card } from "antd";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import DeletePerson from "../buttons/DeletePerson";
+import UpdatePerson from "../form/UpdatePerson";
+import "./PersonCard.css";
 
 const PersonCard = (props) => {
-  const { firstName, lastName } = props;
+  const { id, firstName, lastName } = props;
+
+  const [editMode, setEditMode] = useState(false);
+
+  const handleButtonClick = () => {
+    setEditMode(!editMode);
+  };
+
   return (
     <>
-      <Card actions={[<DeletePerson id={props.id} />]}>
-        {firstName} {lastName}
-      </Card>
+      {editMode ? (
+        <UpdatePerson
+          id={id}
+          firstName={firstName}
+          lastName={lastName}
+          onButtonClick={handleButtonClick}
+        />
+      ) : (
+        <Card
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gridTemplateRows: "1fr 1fr",
+            alignItems: "center",
+            justifyContent: "center",
+            border: "2px solid #5603AD",
+            fontSize: "20px",
+            padding: "10px",
+          }}
+          actions={[
+            <EditOutlined key="edit" onClick={handleButtonClick} />,
+            <DeletePerson id={props.id} />,
+          ]}
+          extra={
+            <Link to={`/details/${id}`}>
+              <Button
+                type="primary"
+                style={{
+                  display: "inline-block",
+                  borderRadius: "5px",
+                  border: "1px solid #5603AD",
+                  padding: "10px",
+                  backgroundColor: "#5603AD",
+                  color: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "120px",
+                  height: "40px",
+                  margin: "20px 0 0 0",
+                }}
+              >
+                Show More
+              </Button>
+            </Link>
+          }
+        >
+          {firstName} {lastName}
+        </Card>
+      )}
     </>
   );
 };
